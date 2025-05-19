3382792f0c8d96da18916608ffba656772ccb958
wilyJ80 <abjurandam@gmail.com>
2025-05-19 09:06:27 -0300

    feat: add daisyui

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ export const app = express();
 app.set('view engine', 'ejs');
 app.engine('html', ejs.renderFile);
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
 		res.render('index.html');
 });
